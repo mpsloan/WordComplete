fe72596d8282944e11b1f094c9a35ebd787e3bb8
mpsloan <97258032+mpsloan@users.noreply.github.com>
2023-05-02 13:48:25 -0400

    Update and rename project5.cpp to main.cpp

--- main.cpp
+++ main.cpp
@@ -1,16 +1,5 @@
-/* 
-Michael Sloan
-Project #5 - Word Autocomplete using an Alphabet Trie
-This project implements an alphabet trie with various methods
-located in the Trie.cpp and Trie.h files. A trie object is made
-up of an array of TrieNode pointers that points to the next node
-in the word. Rather than storing characters in these nodes, the index
-of the array plus the ascii value of the char 'a' can tell you what
-character is being stored in that node. This main program is a word 
-autocomplete application that prompts the user to enter a prefix, and
-with the methods written in Trie.cpp, the program tells the user how many
-words in the "wordlist.txt" file start with that prefix.
-*/
+// main program
+
 #include "Trie.h"
 #include <iostream>
 #include <string>
